856a8cc0b2f4c6d74158c163b48b34e5cd299fa8
Astavie <astavie@pm.me>
2023-04-11 10:39:01 +0200

    canvas value and canvas signal

--- src/canvas.lua
+++ src/canvas.lua
@@ -1,11 +1,29 @@
 --- @meta
 
+---@class Hint
+
+---@class canvas
+---@field TIME Hint
+---@field SIZE Hint
+---@field VEC2 Hint
 local canvas = {}
 
 ---@param text string
 ---@param font string?
 ---@return number
 function canvas.measure(text, font)
+end
+
+---@param name string
+---@param hint Hint
+---@param parent? string
+---@return fun(): any
+function canvas.value(name, hint, parent)
+end
+
+---@param name string
+---@return (fun(): any)?
+function canvas.signal(name)
 end
 
 return canvas
